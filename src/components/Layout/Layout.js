8ffec42e417ff19a666b384b33e6d23f5fbd63f4
Kirtana Iwamoto <sqvishee@gmail.com>
2020-07-29 18:50:42 -0500

    Build Controls and Order Summary

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,7 +4,6 @@ import classes from './Layout.module.css';
 
 const layout = ( props ) => (
     <Aux>
-    <div>Toolbar, Sidebar, Backdrop</div>
     <main className={classes.Content}>
         {props.children}
     </main>
